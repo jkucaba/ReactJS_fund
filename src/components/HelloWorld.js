d081c8b2c0192bb333e2b11cdd9bdfb3027dc44d
Jakub <jakub.kucaba@gmail.com>
2024-07-26 17:49:03 +0200

    Accessing javascript variables in JSX

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -6,11 +6,16 @@ const HelloWorld = () => {
     function handleClick(){
         alert("Buton clicked");
     }
+    
+    const name = "Jakub"
+    const addition = 1 + 2;
 
     return (<div>
         <h1 className='title'>Title</h1>
         <h2>Sub title</h2>
         <p>paragraph</p>
+        <p>{name}</p>
+        <p>{addition}</p>
         <button onClick={handleClick}>Click</button>
     </div>)
 }
