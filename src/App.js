6bfa4df3f019455e1ea4eef4d9e861cf36979bde
Jakub <jakub.kucaba@gmail.com>
2024-07-30 18:57:12 +0200

    Event Handling

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import HelloWorld from './components/HelloWorld';
 import Student from './components/Student';
 import Employee from './components/Employee';
 import User from './components/User';
+import EventHandling from './components/EventHandling';
 
 function App() {
 
@@ -46,8 +47,10 @@ const skills = ["Java", "Kotlin", "SpringBoot"]
 
       {/* <Employee/> */}
 
-      <User
-      />
+      {/* <User
+      /> */}
+    <EventHandling/>
+    
     </div>
   );
 }
