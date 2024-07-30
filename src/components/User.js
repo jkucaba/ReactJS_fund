05483faa60b166915bec74a60ed18cb604ef3dd4
Jakub <jakub.kucaba@gmail.com>
2024-07-30 18:50:00 +0200

    useState Hook v2

--- src/components/User.js
+++ src/components/User.js
@@ -2,22 +2,31 @@ import { useState } from "react"
 
 const User  = () => {
     
-    const [firstName, setFirstName] = useState('Jakubson')
-    const [lastName, setLastName] = useState('Kubica')
-    const [email, setEmail] = useState('jkubica@gmail.com')
+    // const [firstName, setFirstName] = useState('Jakubson')
+    // const [lastName, setLastName] = useState('Kubica')
+    // const [email, setEmail] = useState('jkubica@gmail.com')
 
+    const [user, setUser] = useState({
+        firstName: 'Jakub',
+        lastName: 'Kubica',
+        email: 'jkubica@gamilc.com'
+    })
+
+    // function updateUser(){
+    //     setFirstName('Nati')
+    //     setLastName('Miki')
+    //     setEmail('nmiki@gmail.com')
+    // }
     function updateUser(){
-        setFirstName('Nati')
-        setLastName('Miki')
-        setEmail('nmiki@gmail.com')
+        setUser({firstName: "Nati", lastName: "Miki", email: "nmiki@gmail.com"})
     }
 
     return (
         <div>
             <h1>User Details</h1>
-            <p>{firstName}</p>
-            <p>{lastName}</p>
-            <p>{email}</p>
+            <p>{user.firstName}</p>
+            <p>{user.lastName}</p>
+            <p>{user.email}</p>
 
             <button onClick={updateUser}>Update User</button>
         </div>
